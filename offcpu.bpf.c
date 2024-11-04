d60de15dbea42a3ea7cd7510f83bdc07d3b82493
Yanming Wei <yw4439@columbia.edu>
2024-11-03 21:07:53 -0500

    test

--- offcpu.bpf.c
+++ offcpu.bpf.c
@@ -20,22 +20,22 @@ struct {
     __type(value, u64);
 } start_time SEC(".maps");
 
-static int trace_sched_switch(struct task_struct *prev, struct task_struct *next) {
+static int trace_sched_switch(u32 prev_pid, u32 next_pid, long prev_state) {
     u64 ts = bpf_ktime_get_ns();
-    u32 prev_pid = BPF_CORE_READ(prev, pid);
-    u32 next_pid = BPF_CORE_READ(next, pid);
     u64 *start_ts, delta;
 
-    long prev_state = BPF_CORE_READ(prev, __state);
+    // If the previous task was running, record its start time
     if (prev_state == TASK_RUNNING) {
         bpf_map_update_elem(&start_time, &prev_pid, &ts, BPF_ANY);
     }
 
+    // Check if the next task's start time is stored
     start_ts = bpf_map_lookup_elem(&start_time, &next_pid);
     if (start_ts) {
         delta = ts - *start_ts;
-        u64 key = delta / 1000;
+        u64 key = delta / 1000;  // Convert to microseconds
 
+        // Look up the corresponding histogram bucket
         u64 *count = bpf_map_lookup_elem(&offcpu_histogram, &key);
         if (count) {
             (*count)++;
@@ -44,21 +44,24 @@ static int trace_sched_switch(struct task_struct *prev, struct task_struct *next
             bpf_map_update_elem(&offcpu_histogram, &key, &init_val, BPF_ANY);
         }
 
+        // Clean up: remove the start time after use
         bpf_map_delete_elem(&start_time, &next_pid);
     }
 
     return 0;
 }
 
-// Use the standard tracepoint for sched_switch
 SEC("tracepoint/sched/sched_switch")
 int handle_sched_switch(struct trace_event_raw_sched_switch *ctx) {
-    struct task_struct *prev = (struct task_struct *)ctx->prev;
-    struct task_struct *next = (struct task_struct *)ctx->next;
-    return trace_sched_switch(prev, next);
+    u32 prev_pid = ctx->prev_pid;  // Get PID of the previous task
+    u32 next_pid = ctx->next_pid;  // Get PID of the next task
+    long prev_state = ctx->prev_state;  // Get the state of the previous task
+
+    return trace_sched_switch(prev_pid, next_pid, prev_state);
 }
 
 char LICENSE[] SEC("license") = "GPL";
+
 
 
 // SEC("raw_tp/sched_switch")
