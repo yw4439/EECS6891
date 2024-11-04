8bbf6180cb8b96c5c894c694ed59496e336ce082
Yanming Wei <yw4439@columbia.edu>
2024-11-03 20:38:58 -0500

    test

--- userspace_loader.c
+++ userspace_loader.c
@@ -7,6 +7,8 @@
 
 int main(int argc, char **argv) {
     struct bpf_object *obj;
+    struct bpf_program *prog;
+    struct bpf_link *link;
     int prog_fd;
 
     // Load the BPF object file
@@ -24,13 +26,14 @@ int main(int argc, char **argv) {
 
     // Get the file descriptor of the loaded BPF program
     prog_fd = bpf_program__fd(bpf_object__find_program_by_name(obj, "handle_sched_switch"));
+    prog = bpf_object__find_program_by_name;
     if (prog_fd < 0) {
         fprintf(stderr, "Failed to find BPF program: %s\n", strerror(errno));
         return 1;
     }
 
     // Attach the BPF program to the sched:sched_switch tracepoint
-    if (bpf_program__attach(prog_fd) < 0) {
+    if (bpf_program__attach(prog) < 0) {
         fprintf(stderr, "Failed to attach BPF program to tracepoint: %s\n", strerror(errno));
         return 1;
     }
